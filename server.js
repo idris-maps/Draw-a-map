411979491bc29b679016e5e1f4e955a9def2223e
anders <anders@hardabud.com>
2015-05-18 19:24:49 +0200

    cleanup

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var app = express();
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/img', express.static(__dirname + '/public/img'));
 app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/views', express.static(__dirname + '/public/views'));
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/index.html');
